f9aa216a845606b13040ccd100c2d1521f9c2166
Enablex <pankaj.p@vcloudx.com>
2023-12-14 10:21:29 +0530

    update uikit sdk

--- lib/pages/video_call.dart
+++ lib/pages/video_call.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:enx_uikit_flutter/enx_uikit_flutter.dart';
 import 'package:enx_uikit_flutter/utilities/enx_setting.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -16,42 +17,53 @@ class VideoCall extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     var setting = EnxSetting.instance;
-    setting.createBottomOption(BottomOption.audio);
-    setting.createBottomOption(BottomOption.video);
-    setting.createBottomOption(BottomOption.groupChat);
-    setting.createBottomOption(BottomOption.disconnect);
-   setting.createBottomOption(BottomOption.cameraSwitch);
 
-    setting.createTopOption(TopOption.userList);
-    setting.createTopOption(TopOption.requestFloor);
-    setting.createTopOption(TopOption.menu);
+    Set<EnxRequiredEventsOption> enxRequired= <EnxRequiredEventsOption>{};
+    enxRequired.addAll([EnxRequiredEventsOption.audio,EnxRequiredEventsOption.video,EnxRequiredEventsOption.cameraSwitch,EnxRequiredEventsOption.audioSwitch,EnxRequiredEventsOption.groupChat,EnxRequiredEventsOption.disconnect]);
+    setting.configureRequiredEventList(enxRequired);
+    setting.isPreScreening(false);
 
     return Scaffold(
-      body: SafeArea(child: EnxVideoView(token: token, connectError: (Map<dynamic, dynamic> map) {
-        print('connectError' + jsonEncode(map));
-        Fluttertoast.showToast(
-            msg: "connectError+${jsonEncode(map)}",
-            toastLength: Toast.LENGTH_SHORT,
-            gravity: ToastGravity.CENTER,
 
-            backgroundColor: Colors.red,
-            textColor: Colors.white,
-            fontSize: 16.0
-        );
-        Get.back();
-      },
-        disconnect: (Map<dynamic, dynamic> map) {
+      body: SafeArea(
+        child: EnxVideoView(
+          token: token,
+
+          connectError: (Map<dynamic, dynamic> map) {
+            if (kDebugMode) {
+              print('connectError${jsonEncode(map)}');
+            }
+            Fluttertoast.showToast(
+                msg: "connectError+${jsonEncode(map)}",
+                toastLength: Toast.LENGTH_SHORT,
+                gravity: ToastGravity.CENTER,
+                backgroundColor: Colors.red,
+                textColor: Colors.white,
+                fontSize: 16.0);
+            Get.back();
+          },
+          disconnect: (Map<dynamic, dynamic> map) {
+            Fluttertoast.showToast(
+                msg: "disconnect+${jsonEncode(map)}",
+                toastLength: Toast.LENGTH_SHORT,
+                gravity: ToastGravity.CENTER,
+                backgroundColor: Colors.red,
+                textColor: Colors.white,
+                fontSize: 16.0);
+            Get.back();
+          }, onPageSlide: (EnxPageSlideEventName enxPageSlideEventName, bool isShow) {
           Fluttertoast.showToast(
-              msg: "disconnect+${jsonEncode(map)}",
+              msg: "onPageSlide+${enxPageSlideEventName.name} $isShow",
               toastLength: Toast.LENGTH_SHORT,
               gravity: ToastGravity.CENTER,
-
               backgroundColor: Colors.red,
               textColor: Colors.white,
-              fontSize: 16.0
-          );
-          Get.back();
-        }, embedUrl: '',   ),),
+              fontSize: 16.0);
+        }, onUserDataReceived: (Map<dynamic, dynamic> map) {
+
+        }, embedUrl: '',
+        ),
+      ),
     );
   }
   
